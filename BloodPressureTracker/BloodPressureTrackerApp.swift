6f8edf5af13465302a077c167e05347f186cdb69
Oracso <85285983+Oracso@users.noreply.github.com>
2024-02-05 09:54:10 +0000

    Initial Commit

---
+++ BloodPressureTracker/BloodPressureTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BloodPressureTrackerApp.swift
+//  BloodPressureTracker
+//
+//  Created by Oscar Hardy on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct BloodPressureTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
